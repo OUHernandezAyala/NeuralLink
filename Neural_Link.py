38a142cad273154389093789c678763efcbe94f8
OUHernandezAyala <142889499+OUHernandezAyala@users.noreply.github.com>
2024-03-18 21:57:46 +0000

    My first Neural Link

--- Neural_Link.py
+++ Neural_Link.py
@@ -1,11 +1,19 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
-capa = tf.keras.layers.Dense(units=1, input_shape=[1])
-modelo =tf.keras.Sequential([capa])
+#capa = tf.keras.layers.Dense(units=1, input_shape=[1])
+#modelo =tf.keras.Sequential([capa])
+
+oculta1 = tf.keras.layers.Dense(units=3, input_shape=[1])
+oculta2 = tf.keras.layers.Dense(units=3)
+salida = tf.keras.layers.Dense(units=1)
+modelo = tf.keras.Sequential([oculta1, oculta2, salida])
+
 
 modelo.compile(
     optimizer=tf.keras.optimizers.Adam(0.1),
@@ -16,3 +24,22 @@ print("Entrenamiento...")
 
 historial= modelo.fit(celsius, fahrenheit, epochs=1000, verbose=False)
 print("Modelo Entrenado")
+
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud de Perdida")
+plt.plot(historial.history["loss"])
+plt.savefig("loss_plot.png")
+
+
+print("Prediccion")
+
+resultado = modelo.predict(np.array([[100.0]]))
+
+
+print("El resultado es"+ str(resultado) + "F")
+
+
+print("Variables internas del modelo")
+print(oculta1.get_weights())
+print(oculta2.get_weights())
+print(salida.get_weights())
